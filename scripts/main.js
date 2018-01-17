55aee32b43e1a8da75c3fe9130e608939f03fe50
James Huynh <jumz.huynh@mail.utoronto.ca>
2018-01-17 01:32:17 -0500

    Modified copyright date

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var len = document.lastModified.length;
 var date = document.lastModified.substring(0,len-8).replace(/ /g,'').split('/');
+var year = date[2];
 date = date[2] + '-' + date[0] + '-' + date[1];
 document.getElementById("lastModified").innerHTML = "Last modified: " + date;
+document.getElementById("copyright").innerHTML = year;
 
 setTimeout(function(){
     document.body.className="";
