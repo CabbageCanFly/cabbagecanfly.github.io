d56d2521ad0bc2124dba8942a95c9918bb734eeb
James Huynh <jumz.huynh@mail.utoronto.ca>
2018-10-06 02:36:44 -0400

    Code refactoring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 main();
 
 let curInp = "";
@@ -19,16 +20,18 @@ function main() {
 }
 
 function setSearch() {
-	let search = document.getElementById("search");
-	search = search.childNodes[1];
+	let searchElem = document.getElementById("search");
+	searchElem = searchElem.childNodes[1];
 
+	// Triggered on letters, numbers, and numpad numbers;
+	// 	does not trigger on ctrl/alt/meta key combos
 	window.onkeydown = function (e) {
 		if (!e) e = window.event;
 		if (!e.metaKey && !e.ctrlKey && !e.altKey) {
 			if((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 48 && 
 				e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || 
 				e.keyCode == 8 || e.keyCode == 46) {
-				search.focus();
+				searchElem.focus();
 			}
 		}
 	};
@@ -38,7 +41,7 @@ function setSearch() {
 			if((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 48 && 
 				e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || 
 				e.keyCode == 8 || e.keyCode == 46) {
-				checkSearch(search);
+				checkSearch(searchElem);
 			}
 		}
 	};
@@ -50,6 +53,8 @@ function checkSearch(input) {
 	} else {
 		curInp = input.value;
 	}
+
+	// Remove previously found tags
 	let elems = document.getElementsByClassName("found");
 	while (elems.length) {
 		let elem = elems[0];
@@ -59,80 +64,79 @@ function checkSearch(input) {
 		}
 		elem.remove();
 	}
+
+	// Reset the courses HTML
 	articles[1].innerHTML = originalContent[1];
 
+	// Check if input is valid; search gets buggy with some punctuation
+	// 	so punctuation is invalid for input.
 	let value = input.value;
 	let regex = new RegExp('^[a-zA-Z0-9_ ]+$');
 	if (regex.test(value) == false) {
+		// Have to reapply event listeners to accordion due to DOM changes
 		setAccordion();
 		return;
 	}
 
 	for (let articleInd = 0; articleInd < articles.length; articleInd++) {
-	// get the html text
-	let html_content = articles[articleInd].innerHTML;
-	// split value into words
-	let value_words = value.split(' ');
-	let nr_words = value_words.length;
+		let content = articles[articleInd].innerHTML;
+		let words = value.split(' ');
+		let numWords = words.length;
 
-	// check if value is inside a html tag or not
-	// if the first angle bracket is a '<' the value is outside of a html tag
-	if (nr_words === 1) {
-		regex = new RegExp(value+'(?=[^>]*?(<|$))','gi');
-	} else {
-		// there can be a html tag between two words
-		regex = value_words[0]+'(?=[^>]*?(<|$))';
-		for (let i = 1; i < nr_words; i++) {
-			 // there can be a space and a whole html tag between two parts 
-			 regex += '(?: ?)(?:<[^>]*?>)?(?: ?)'+value_words[i]+'(?=[^>]*?(<|$))';     
+		// Check if input is in an HTML tag
+		if (numWords === 1) {
+			regex = new RegExp(value+'(?=[^>]*?(<|$))','gi');
+		} else {
+			// HTML tag can be in between words
+			regex = words[0]+'(?=[^>]*?(<|$))';
+			for (let i = 1; i < numWords; i++) {
+				 regex += '(?: ?)(?:<[^>]*?>)?(?: ?)' + words[i] + '(?=[^>]*?(<|$))';     
+			}
+			regex = new RegExp(regex,'gi');
 		}
-		regex = new RegExp(regex,'gi');
+
+		let matches = null;
+		let positions = []
+		let found = []
+		let addFound = [];
+
+		while (matches = regex.exec(content)) {
+			let startTag = '<span class="found">';
+			let endTag = '</span>';
+			// If the match contains html tags there need to be a </span>
+			// 	before the html tag starts and a <span> if the html tags
+			// 	closes again
+			// abc <p>def => abc </span><p><span class="found">def</span>   
+			let match = matches[0].replace(/>/g,'>'+startTag);
+			match = match.replace(/<(?!span class="found">)/g,endTag+'<');
+
+			// Save how many chars have been added 
+			addFound.push(match.length-matches[0].length);
+
+			found.push(match); 
+			positions.push(matches.index); 
+		}
+
+		let numAdded = 0;
+		let newContent = content;
+		// Iterate through all positions and add a span tag to mark the query
+		for (let i = 0; i < positions.length; i++) {
+			let startTag = '<span class="found" id="found_'+i+'">';
+			let endTag = '</span>';
+
+			let contentBefore = newContent.substr(0, positions[i] + numAdded);
+			let foundTag = startTag + found[i] + endTag;
+			let contentAfter = newContent.substr(positions[i] + numAdded +
+					found[i].length - addFound[i]);
+
+			numAdded += startTag.length + endTag.length + addFound[i];
+			newContent = contentBefore + foundTag + contentAfter;
+		}
+		articles[articleInd].innerHTML = newContent;
 	}
 
-	let matches = null;
-	let positions = [], found = [], add_found_char = [];
-
-	while(matches = regex.exec(html_content)) {
-		// add this before / after a value part
-		let start_tag = '<span class="found">';
-		let end_tag = '</span>';
-		// if the match contains html tags there need to be a </span>
-		// before the html tag starts and a <span if the html tags closes again
-		// abc <p>def => abc </span><p><span class="found">def</span>   
-		let match = matches[0].replace(/>/g,'>'+start_tag);
-		match = match.replace(/<(?!span class="found">)/g,end_tag+'<');
-			
-		// save the new match with correct html tags    
-		found.push(match); 
-		// save how many chars have been added 
-		add_found_char.push(match.length-matches[0].length);
-		// save found position
-		positions.push(matches.index); 
-	}
-
-	let add_nr_chars = 0;
-	let new_html_content = html_content;
-	// iterate through all positions and add a span tag to mark the query
-	for (let i = 0; i < positions.length; i++) {
-		// add this before / after the found value
-		let start_tag = '<span class="found" id="found_'+i+'">';
-		let end_tag = '</span>';
-		// string before value
-		let content_before = new_html_content.substr(0,positions[i]+add_nr_chars);
-		// value and start_tag and end_tag
-		let value_and_tags = start_tag + found[i] + end_tag;
-		// string after value
-		let content_after = new_html_content.substr(positions[i]+add_nr_chars+found[i].length-add_found_char[i]);
-
-		// number of characters which have been added
-		add_nr_chars += start_tag.length + end_tag.length + add_found_char[i];
-		
-		// new content
-		new_html_content = content_before + value_and_tags + content_after;
-	}
-	articles[articleInd].innerHTML = new_html_content;
-	}
-
+	// Traverse through found tags, specifically in courses.
+	// Expand the accordion that contains the found content.
 	elems = document.getElementsByClassName("found");
 	for (let i = 0; i < elems.length; i++) {
 		elem = elems[i];
@@ -151,78 +155,72 @@ function checkSearch(input) {
 			}
 			elem = elem.parentNode;
 		}
+		// Toggle the radio button to the first article content that contains
+		// 	a found tag
 		if (i == 0) {
 			document.getElementById("btn_c1_" + elem.id).checked = true;
 		}
 	}
 
-	// checkAccordion() called twice, with a slight delay, to allow
-	// 	accordion panel to properly expand completely.
-	// setAccordion() re-adds event listener to each accordion due to HTML DOM
-	//  overwriting.
-	checkAccordion();
+	// setAccordion() re-adds event listener to each accordion due to
+	// 	DOM changes
+	checkAccordion(400);
 	setAccordion();
-	setTimeout(checkAccordion, 400);
 }
 
+// Adds event listeners to accordion for toggling class.
 function setAccordion() {
 	let acc = document.getElementsByClassName("accordion");
 	let i;
 	for (i = 0; i < acc.length; i++) {
 		acc[i].addEventListener("click", function() {
-			let parent;
-			let panel = this.nextElementSibling;
-			let height;
-			let pheight;
-			if (panel.style.maxHeight || hasClass(this, "accordion-active")){
-				panel.style.maxHeight = null;
-			} else {
-				height = panel.scrollHeight * 1.2;
-				panel.style.maxHeight = height + "px";
-				parent = panel.parentNode;
-				while (parent != document) {
-					if (hasClass(parent, "panel")) {
-						height = (parent.scrollHeight + panel.scrollHeight) * 1.2;
-						pheight = parent.style.maxHeight.replace("px","");
-						if (+pheight < height) {
-							parent.style.maxHeight = height + "px";
-						}
-					}
-					parent = parent.parentNode;
-				}
-			}
-
-			this.classList.toggle("accordion-active");
+			toggleAccordion(this, true);
 		});
 	}
 }
-function checkAccordion() {
+
+// checkAccordion() called twice, with a slight delay, to allow
+// 	accordion panel to properly expand completely; used mainly
+// 	during search
+function checkAccordion(delay) {
+	if (delay > 0) {
+		setTimeout(function() {checkAccordion(0)}, delay);
+	}
 	let acc = document.getElementsByClassName("accordion");
-	let i;
-	for (i = 0; i < acc.length; i++) {
-		let elem = acc[i];
-		let parent;
-		let panel = elem.nextElementSibling;
-		let height;
-		let pheight;
-		if (!hasClass(elem, "accordion-active")){
-			panel.style.maxHeight = null;
-		} else {
-			height = panel.scrollHeight * 1.2;
-			panel.style.maxHeight = height + "px";
-			parent = panel.parentNode;
-			while (parent != document) {
-				if (hasClass(parent, "panel")) {
-					height = (parent.scrollHeight + panel.scrollHeight) * 1.2;
-					pheight = parent.style.maxHeight.replace("px","");
-					if (+pheight < height) {
-						parent.style.maxHeight = height + "px";
-					}
+	for (let i = 0; i < acc.length; i++) {
+		toggleAccordion(acc[i]);
+	}
+}
+
+// Compute/toggle next height of panel depending on state of accordion
+function toggleAccordion(elem, toggle) {
+	if (toggle != true) {
+		toggle = false;
+	}
+	let parent;
+	let panel = elem.nextElementSibling;
+	let height;
+	let pheight;
+	if (hasClass(elem, "accordion-active") == toggle){
+		panel.style.maxHeight = null;
+	} else {
+		height = panel.scrollHeight * 1.2;
+		panel.style.maxHeight = height + "px";
+		parent = panel.parentNode;
+		while (parent != document) {
+			if (hasClass(parent, "panel")) {
+				height = (parent.scrollHeight + panel.scrollHeight) * 1.2;
+				pheight = parent.style.maxHeight.replace("px","");
+				if (+pheight < height) {
+					parent.style.maxHeight = height + "px";
 				}
-				parent = parent.parentNode;
 			}
+			parent = parent.parentNode;
 		}
-	}	
+	}
+	if (toggle) {
+		elem.classList.toggle("accordion-active");
+	}
 }
 
 function hasClass(element, className) {
